757844e610d36d50cd87b4ec11a49ae82f9dd72b
Alexander Rampp <252780+arampp@users.noreply.github.com>
2023-10-18 17:40:14 +0200

    WIP

--- lib.ts
+++ lib.ts
@@ -1,3 +1,7 @@
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+
 export function sanitize(entry: string) {
-    return entry;
+    const parser = new DOMParser();
+    const html = parser.parseFromString(entry, 'text/html');
+    return html?.children[0].innerHTML;
 }
